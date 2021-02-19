1f27ed3a73db479a857eead092b65af24291139c
Miguel Storani <storanimiguel1998+gitkraken@gmail.com>
2021-02-19 17:22:16 -0300

    chore(style): change some styles to match SouthXchange style

--- src/plugins/topbar/topbar.jsx
+++ src/plugins/topbar/topbar.jsx
@@ -2,7 +2,7 @@ import React, { cloneElement } from "react"
 import PropTypes from "prop-types"
 
 //import "./topbar.less"
-import Logo from "./logo_small.svg"
+import Logo from "./logo-x.png"
 import {parseSearch, serializeSearch} from "../../core/utils"
 
 export default class Topbar extends React.Component {
@@ -136,9 +136,10 @@ export default class Topbar extends React.Component {
       <div className="topbar">
         <div className="wrapper">
           <div className="topbar-wrapper">
-            <Link>
               <img height="40" src={ Logo } alt="Swagger UI"/>
-            </Link>
+              <span className="title">
+                SouthXchange API
+              </span>
             <form className="download-url-wrapper" onSubmit={formOnSubmit}>
               {control.map((el, i) => cloneElement(el, { key: i }))}
             </form>
